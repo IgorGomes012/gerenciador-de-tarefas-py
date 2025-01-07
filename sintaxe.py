ff4a285f3aa578d7d597ba474d951a9b9a168976
igor gomes <gomesoliveirasl02@hotmail.com>
2025-01-06 21:05:46 -0300

    aprendendo sintaxe

---
+++ sintaxe.py
@@ -0,0 +1,15 @@
+# comentararios 
+
+"""
+    Comenntarios multiplas linhas
+"""
+
+
+nome = "Igor" + "Gomes"
+
+idade = 25
+
+if idade < 18:
+   print("menor de idade")
+else:
+   print("maior de idade")      
